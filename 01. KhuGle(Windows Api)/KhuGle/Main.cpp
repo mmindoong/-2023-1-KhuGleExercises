26288fc6825d42269290d71e2a76b2b1172d1d28
mmindoong <san012@khu.ac.kr>
2023-03-28 00:26:41 +0900

    Initial Commit
    Project Setting

---
+++ 01. KhuGle(Windows Api)/KhuGle/Main.cpp
@@ -0,0 +1,28 @@
+//
+//	Dept. Software Convergence, Kyung Hee University
+//	Prof. Daeho Lee, nize@khu.ac.kr
+//
+#include "KhuGleWin.h"
+#include <iostream>
+
+#pragma warning(disable:4996)
+
+#define _CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+#ifndef DBG_NEW
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#define new DBG_NEW
+#endif
+#endif  // _DEBUG
+
+int main()
+{
+	CKhuGleWin *pKhuGleSample = new CKhuGleWin(640, 480);
+
+	KhuGleWinInit(pKhuGleSample);
+
+	return 0;
+}
